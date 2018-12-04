43658834d2a5034c1b2d9761b9867f7f4162fdd5
Don Ward <10is0420@gmail.com>
2018-12-04 05:26:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,45 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require pry
+
+def roll_call_dwarves(dwarf)
+  
+  dwarf.each_with_index{ |name , index|
+  
+    index = index + 1
+    
+    puts "#{index}. #{name}"
+    
+  }
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+binding.pry 
+
+def summon_captain_planet(veggies)
+  
+  captain = []
+  
+  i = 0
+  
+  while i < veggies.length
+  
+    captain << yield(collection[i])
+    
+    i = i + 1
+    
+  end
+  
+  if captain.include?("apples")
+    
+    captain.capitalize
+    
+  else captain.include?("Carrot!")
+    
+    captain
+    
+  end
+  
+end  
+
 end
 
 def long_planeteer_calls# code an argument here
