5d8e49922103487af6a8bc9a90a534f3ff46657d
Don Ward <10is0420@gmail.com>
2018-12-07 18:09:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarf)
+  
+  dwarf.each_with_index{ |name , index|
+  
+    index = index + 1
+    
+    puts "#{index}. #{name}"
+    
+  }
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(planeteer_calls)
+  
+  planeteer_calls.collect {|phrase| phrase.capitalize + "!"}
+  
+  #captain = []
+  
+  #i = 0
+  
+  #while i < veggies.length
+  
+    #captain << yield(collection[i])
+    
+    #i = i + 1
+    
+  #end
+  
+end  
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
+    calls_long.collect {|i| i.count < 4}
+    
 end
 
 def find_the_cheese# code an argument here
